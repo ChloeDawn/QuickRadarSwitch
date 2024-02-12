510e652b8892ce4fa4000cb2fd9e6b22796f4289
Chloe Dawn <chloe@sapphic.dev>
2024-02-12 20:51:04 +0000

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,45 @@
+﻿using BepInEx;
+using LethalCompanyInputUtils.Api;
+using UnityEngine.InputSystem;
+
+namespace HeckinChloe.QuickRadarSwitch;
+
+[BepInPlugin("HeckinChloe.QuickRadarSwitch", "QuickRadarSwitch", "1.0.0")]
+[BepInDependency("com.rune580.LethalCompanyInputUtils", "0.6.3")]
+public sealed class Plugin : BaseUnityPlugin
+{
+    private sealed class InputActions : LcInputActions
+    {
+        [InputAction("<Keyboard>/leftArrow", Name = "Previous Target")]
+        internal InputAction PreviousTarget { get; set; }
+
+        [InputAction("<Keyboard>/rightArrow", Name = "Next Target")]
+        internal InputAction NextTarget { get; set; }
+    }
+
+    private static readonly InputActions Inputs = new();
+
+    private void Awake()
+    {
+        Inputs.PreviousTarget.performed += _ => TrySwitchRadarTarget(step: -1);
+        Inputs.NextTarget.performed += _ => TrySwitchRadarTarget(step: 1);
+    }
+
+    private void TrySwitchRadarTarget(int step)
+    {
+        var player = GameNetworkManager.Instance.localPlayerController;
+
+        if ((player != null) && player.inTerminalMenu)
+        {
+            var mapScreen = StartOfRound.Instance.mapScreen;
+            var currentIndex = mapScreen.targetTransformIndex;
+            var targetCount = mapScreen.radarTargets.Count;
+            var newIndex = (currentIndex + step + targetCount) % targetCount;
+
+            if (newIndex != mapScreen.targetTransformIndex)
+            {
+                mapScreen.SwitchRadarTargetAndSync(newIndex);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Quick Radar Switch
+A mod for Lethal Company that allows for switching the radar target in the terminal with keybinds.
+
+| Keybind         | Default       |
+|-----------------|---------------|
+| Previous Target | `Left Arrow`  |
+| Next Target     | `Right Arrow` |
